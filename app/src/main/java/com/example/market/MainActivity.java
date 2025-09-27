a0c9e6be76473adaf2fb6a2b17507e3c1eb59fe6
bylsan <bulsanomar12@gmail.com>
2025-09-27 08:07:48 +0300

    bmi update

--- app/src/main/java/com/example/market/MainActivity.java
+++ app/src/main/java/com/example/market/MainActivity.java
@@ -8,9 +8,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.view.View;/////
+
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/////
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etHeigh, etWeight;
+    Button btCalculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +31,42 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        connectComponents();
+    }
+
     public void gotoNext(View view) {
         Intent i = new Intent(this,NestActivity.class);
         startActivity(i);
+
+    }
+
+    private void connectComponents(){
+        etHeigh = findViewById(R.id.etHeigh);
+        etWeight = findViewById(R.id.etWeight);
+        btCalculate = findViewById(R.id.btCalculate);
+    }
+
+    public void calculate (View view) {
+        String weight = etWeight.getText().toString();
+        String heigh = etHeigh.getText().toString();
+
+        if (weight.isEmpty() || heigh.isEmpty() ) {
+            Toast.makeText(this, "must only contain numbers", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        double w,h,bmi;
+        w = Double.parseDouble(weight);
+        h = Double.parseDouble(heigh);
+        bmi = w / (h*h);
+
+        Toast.makeText(this, "your bmi is" + bmi, Toast.LENGTH_LONG).show();
     }
 }
